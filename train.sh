b45e17057f0068cbee3f16989e0a85d467fb479e
Sergimayol <sergi2808@gmail.com>
2024-06-22 20:05:15 +0200

    Delete train.sh

--- train.sh
+++
@@ -1,5 +0,0 @@
-# Translation dataset: google/flan-t5-base 
-WANDB=1 WORKERS=$(nproc) DEBUG=2 python src/train.py -m google/flan-t5-base -hr Sergi28/text-2-sql-4-llm -d 'Translation dataset' -tm ./models/bloom.tmodules.json --logging_steps 150 -e 10 --eval_steps 700
-
-# Translation dataset: mrm8488/t5-base-finetuned-wikiSQL
-WANDB=1 WORKERS=$(nproc) DEBUG=2 python src/train.py -m mrm8488/t5-base-finetuned-wikiSQL -hr Sergi28/text-2-sql-4-llm -d 'Translation dataset' -tm ./models/bloom.tmodules.json --logging_steps 150 -e 10 --eval_steps 700
