ff5d588e17b2ed57e207232b71edde16e21b722f
Sergimayol <sergi2808@gmail.com>
2024-08-12 18:03:08 +0200

    rm unused files

--- src/bench.py
+++
@@ -1,8 +0,0 @@
-import os
-from utils import load_json
-
-if __name__ == "__main__":
-    base_path = os.path.join(__file__, "..", "..", "tests")
-    models = load_json(os.path.join(base_path, "models.json"))
-    tests = [load_json(os.path.join(base_path, f"{i}.json")) for i in range(0, 10)]
-    print(tests)
